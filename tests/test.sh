870e264c13a22a01305e6f498fb1a44ec4844986
bing2android <bing2android@gmail.com>
2018-04-01 10:36:16 +1000

    add test-run.yml

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
 echo "generating bart's ssh for testing..."
 rm bart
 ssh-keygen -f bart -N "" && mv bart.pub ./keys && cp ./keys/bart.pub ./keys/joe.pub 
 
-ansible-playbook -i inventory test.yml
+echo "test run..."
+time ansible-playbook -i inventory test-run.yml -C
+echo "real run..."
+time ansible-playbook -i inventory test.yml
 
 echo "remove bart's keys..."
 rm -f bart bart.pub
